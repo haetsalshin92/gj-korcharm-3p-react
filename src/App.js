167c2f4a6b7180aee4d7a5ea5dc0ed1a21e920b5
haetsalshin92 <sinsin09022@gmail.com>
2025-07-16 11:19:57 +0900

    webhook테스트

--- src/App.js
+++ src/App.js
@@ -42,10 +42,10 @@ function App() {
                       overflow: 'hidden',
                       textOverflow: 'ellipsis',
                       background: '#f7f7f7',
-                      padding: '10px',
-                      maxWidth: '100%',
+                      padding: '10px',  
+                      maxWidth: '100%',  
                     }}>
-                      {review.diff.split('\n')[0]}
+                      {review.diff.split('\n')[0]}  
                     </pre>
                   )}
                 </div>
